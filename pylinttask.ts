7ac376f18d6fa14f7f0af594fd81cc88f6047d19
Darren Fuller <daz.fuller@gmail.com>
2017-06-03 09:32:54 +0100

    add some missing commens to code

--- pylinttask.ts
+++ pylinttask.ts
@@ -59,6 +59,7 @@ async function run() {
     
     let lintResults = pyLintTool.execSync(pyLintToolOptions);
 
+    // Check if the output contains mentions of lint checker warnings
     if (lintResults.stdout.match(/\[[A-Z]{1}\d{4}.*\]/g)) {
         console.log(lintResults.stdout);
         tl.setResult(tl.TaskResult.Failed, 'Failed PyLint checks');
